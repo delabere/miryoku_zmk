698abd526fa7bebc296115c94d73292ab76d7637
delabere <jack.rickards@hotmail.co.uk>
2024-01-13 16:02:29 +0000

    define keys using 0 indexed count top left to bottom right, left to right

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -3,8 +3,9 @@
 
 #pragma once
 
-#define KEYS_L K00 K01 K02 K03 K04 K10 K11 K12 K13 K14 K20 K21 K22 K23 K24// left hand
-#define KEYS_R K05 K06 K07 K08 K09 K15 K16 K17 K18 K19 K25 K26 K27 K28 K29 // right hand
-#define THUMBS K32 K33 K34 K35 K36 K37
+#define KEYS_L 1 2 3 4 5 13 14 15 16 17 25 26 27 28 29// left hand
+#define KEYS_R 6 7 8 9 10 18 19 20 21 22 30 31 32 33 34 // right hand
+#define THUMBS 39 40 41 42 43 44
+
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
