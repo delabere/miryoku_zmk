632fe365b58855687b172f688103a1428c66afc0
delabere <jack.rickards@hotmail.co.uk>
2024-01-13 15:45:03 +0000

    move the definitions for the keys into the main .h file

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -26,6 +26,10 @@
 
 #define U_TAPPING_TERM 200
 
+#define KEYS_L K00 K01 K02 K03 K04 K10 K11 K12 K13 K14 K20 K21 K22 K23 K24// left hand
+#define KEYS_R K05 K06 K07 K08 K09 K15 K16 K17 K18 K19 K25 K26 K27 K28 K29 // right hand
+#define THUMBS K32 K33 K34 K35 K36 K37
+
 #include "miryoku_clipboard.h"
 
 #include "miryoku_double_tap_guard.h"
